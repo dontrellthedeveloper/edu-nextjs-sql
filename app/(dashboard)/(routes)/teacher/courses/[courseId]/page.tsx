696f22d2624c9248678659ac3535b2b0a66a6dc6
dontrellthedeveloper <dontrellknight@gmail.com>
2023-09-29 19:27:57 -0400

    added file upload ability

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -6,6 +6,8 @@ import { db } from "@/lib/db";
 import { IconBadge } from "@/components/icon-badge";
 import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
+import { ImageForm } from "./_components/image-form";
+
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth();
@@ -57,6 +59,10 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
             courseId={course.id}
           />
           <DescriptionForm
+            initialData={course}
+            courseId={course.id}
+          />
+          <ImageForm
             initialData={course}
             courseId={course.id}
           />
